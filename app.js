d186d58dec06f0431cf6c58a338783d9222f3f48
MNMatusiak <m.n.matusiak@gmail.com>
2021-10-10 16:31:38 +0200

    Praca

---
+++ app.js
@@ -0,0 +1,11 @@
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", function () {
+
+    if (window.scrollY > 200) {
+        header.classList.add("header-sticky");
+    } else {
+        header.classList.remove("header-sticky")
+    }
+})
+
